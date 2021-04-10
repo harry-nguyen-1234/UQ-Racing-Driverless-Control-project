6a6e7223b12d3ece63cd5f23f15d33168a7c6422
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-04-11 09:52:34 +1000

    Create car controller class Draw car position using rviz Remove python plotting

--- src/car_controller_py/src/car_controller.py
+++ src/car_controller_py/src/car_controller.py
@@ -5,39 +5,55 @@ import numpy as np
 from std_msgs.msg import String
 from fssim_messages.msg import Command, State, WheelSpeed
 from geometry_msgs.msg import PolygonStamped, Point32
+from visualization_msgs.msg import Marker
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
 
+class CarController:
+    def __init__(self):
+        rospy.init_node('car_controller', anonymous=True)
+        self.pub_command = rospy.Publisher(
+            "chicken/command", Command, latch=True, queue_size=1)
 
-def callback_trajectory(data):
-    x = np.array([point.x for point in data.polygon.points])
-    y = np.array([point.y for point in data.polygon.points])
+        self.pub_position = rospy.Publisher(
+            "chicken/position", Marker, latch=True, queue_size=1)
 
+        rospy.Subscriber("chicken/trajectory", PolygonStamped, self.callback_trajectory,
+                         queue_size=1)
 
-def callback_state(data):
-    x = data.position.x
-    y = data.position.y
-    fig.clear()
-    plt.scatter(x, y)
-    plt.xlim([-30, 50])
-    plt.ylim([-80, 10])
-    plt.axes().set_aspect('equal')
-    plt.grid(True)
-    plt.draw()
-    plt.pause(0.01)
+        rospy.Subscriber("chicken/state", State,
+                         self.callback_state, queue_size=1)
+
+    def callback_trajectory(self, data):
+        x = np.array([point.x for point in data.polygon.points])
+        y = np.array([point.y for point in data.polygon.points])
+
+    def callback_state(self, data):
+        x = data.position.x
+        y = data.position.y
+
+        marker = Marker()
+        marker.ns = "position"
+        marker.id = 0
+        marker.header.stamp = rospy.get_rostime()
+        marker.header.frame_id = "map"
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.orientation.w = 1.0
+        marker.scale.x = 0.25
+        marker.scale.y = 0.25
+        marker.scale.z = 0.25
+        marker.color.r = 1.0
+        marker.color.a = 1.0
+
+        self.pub_position.publish(marker)
 
 
 if __name__ == '__main__':
 
-    rospy.init_node('car_controller', anonymous=True)
-
-    rospy.Subscriber("chicken/trajectory", PolygonStamped, callback_trajectory,
-                     queue_size=1)
-    rospy.Subscriber("chicken/state", State, callback_state, queue_size=1)
-
-    rospy.Publisher("chicken/command", Command, latch=True, queue_size=1)
-    plt.show()
+    car_controller = CarController()
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
