e4d99ce9a27d051e4beae23e03231ac0c32b6724
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-04-11 10:31:16 +1000

    Draw the velocity/heading of the car

--- src/car_controller_py/src/car_controller.py
+++ src/car_controller_py/src/car_controller.py
@@ -7,6 +7,7 @@ from fssim_messages.msg import Command, State, WheelSpeed
 from geometry_msgs.msg import PolygonStamped, Point32
 from visualization_msgs.msg import Marker
 import matplotlib.pyplot as plt
+from tf.transformations import quaternion_about_axis
 
 
 class CarController:
@@ -29,25 +30,36 @@ class CarController:
         y = np.array([point.y for point in data.polygon.points])
 
     def callback_state(self, data):
-        x = data.position.x
-        y = data.position.y
+        x_pos = data.position.x
+        y_pos = data.position.y
 
         marker = Marker()
-        marker.ns = "position"
         marker.id = 0
         marker.header.stamp = rospy.get_rostime()
         marker.header.frame_id = "map"
         marker.type = Marker.SPHERE
         marker.action = Marker.ADD
-        marker.pose.position.x = x
-        marker.pose.position.y = y
+        marker.pose.position.x = x_pos
+        marker.pose.position.y = y_pos
         marker.pose.orientation.w = 1.0
         marker.scale.x = 0.25
         marker.scale.y = 0.25
         marker.scale.z = 0.25
         marker.color.r = 1.0
         marker.color.a = 1.0
+        self.pub_position.publish(marker)
 
+        marker.id = 1
+        marker.type = Marker.ARROW
+        marker.scale.x = 2.0
+        marker.scale.y = 0.10
+        marker.scale.z = 0.10
+        q = quaternion_about_axis(
+            data.position.theta + data.velocity.theta, (0, 0, 1))
+        marker.pose.orientation.x = q[0]
+        marker.pose.orientation.y = q[1]
+        marker.pose.orientation.z = q[2]
+        marker.pose.orientation.w = q[3]
         self.pub_position.publish(marker)
 
 
