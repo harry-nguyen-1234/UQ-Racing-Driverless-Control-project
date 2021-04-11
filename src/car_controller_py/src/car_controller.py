7e4c5127fcb8c39a2d67ad04e28b7f6fe7dfbe55
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-04-12 08:36:09 +1000

    Draw lookahead point for pure pursuit

--- src/car_controller_py/src/car_controller.py
+++ src/car_controller_py/src/car_controller.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-import rospy
 import numpy as np
+import math
+from scipy.spatial import KDTree
+import matplotlib.pyplot as plt
+
+import rospy
+from tf.transformations import quaternion_about_axis
 from std_msgs.msg import String
 from fssim_messages.msg import Command, State, WheelSpeed
 from geometry_msgs.msg import PolygonStamped, Point32
 from visualization_msgs.msg import Marker
-import matplotlib.pyplot as plt
-from tf.transformations import quaternion_about_axis
-from scipy.spatial import KDTree
 
 
 class CarController:
@@ -19,8 +21,8 @@ class CarController:
         self.pub_position = rospy.Publisher(
             "chicken/position", Marker, latch=True, queue_size=1)
 
-        rospy.Subscriber("chicken/trajectory", PolygonStamped, self.callback_trajectory,
-                         queue_size=1)
+        rospy.Subscriber("chicken/trajectory", PolygonStamped,
+                         self.callback_trajectory, queue_size=1)
 
         rospy.Subscriber("chicken/state", State,
                          self.callback_state, queue_size=1)
@@ -28,14 +30,22 @@ class CarController:
         self.car_position = None
         self.car_velocity = None
         self.car_acceleration = None
+
         self.traj_coords = None
-        self.traj_closest_x = None
-        self.traj_closest_y = None
+        self.traj_closet_index = 0
+        self.traj_closest_x = 0
+        self.traj_closest_y = 0
         self.tree = None
+
+        self.lookahead_dist = 1
+        self.lookahead_index = 0
+        self.lookahead_x = 0
+        self.lookahead_y = 0
 
         self.MARKER_POSITION = 0
         self.MARKER_HEADING = 1
         self.MARKER_CLOSEST_TRAJ = 2
+        self.MARKER_LOOKAHEAD = 3
 
     def draw_car_position(self, marker):
         marker.id = self.MARKER_POSITION
@@ -44,6 +54,7 @@ class CarController:
 
         marker.pose.position.x = self.car_position.x
         marker.pose.position.y = self.car_position.y
+        marker.pose.position.z = 0
         marker.pose.orientation.x = 0.0
         marker.pose.orientation.y = 0.0
         marker.pose.orientation.z = 0.0
@@ -68,6 +79,7 @@ class CarController:
         q = quaternion_about_axis(self.car_position.theta, (0, 0, 1))
         marker.pose.position.x = self.car_position.x
         marker.pose.position.y = self.car_position.y
+        marker.pose.position.z = 0
         marker.pose.orientation.x = q[0]
         marker.pose.orientation.y = q[1]
         marker.pose.orientation.z = q[2]
@@ -91,6 +103,7 @@ class CarController:
 
         marker.pose.position.x = self.traj_closest_x
         marker.pose.position.y = self.traj_closest_y
+        marker.pose.position.z = 0
         marker.pose.orientation.x = 0.0
         marker.pose.orientation.y = 0.0
         marker.pose.orientation.z = 0.0
@@ -106,6 +119,52 @@ class CarController:
         marker.color.a = 1.0
 
         self.pub_position.publish(marker)
+
+    def draw_lookahead_point(self, marker):
+        marker.id = self.MARKER_LOOKAHEAD
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+
+        marker.pose.position.x = self.lookahead_x
+        marker.pose.position.y = self.lookahead_y
+        marker.pose.position.z = 0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
+
+        marker.scale.x = 0.25
+        marker.scale.y = 0.25
+        marker.scale.z = 0.25
+
+        marker.color.r = 1.0
+        marker.color.b = 1.0
+        marker.color.g = 0.0
+        marker.color.a = 1.0
+
+        self.pub_position.publish(marker)
+
+    def pure_pursuit(self, marker):
+        _, indexes = self.tree.query(
+            [(self.car_position.x, self.car_position.y)])
+        self.traj_closet_index = indexes[0]
+        self.traj_closest_x = self.traj_coords[self.traj_closet_index][0]
+        self.traj_closest_y = self.traj_coords[self.traj_closet_index][1]
+        self.draw_closest_traj_point(marker)
+
+        self.lookahead_index = self.traj_closet_index
+        while(True):
+            self.lookahead_x = self.traj_coords[self.lookahead_index][0]
+            self.lookahead_y = self.traj_coords[self.lookahead_index][1]
+            dist_to_point = math.sqrt((self.car_position.x - self.lookahead_x)**2 +
+                                      (self.car_position.y - self.lookahead_y)**2)
+            if dist_to_point > self.lookahead_dist:
+                break
+
+            self.lookahead_index = (
+                self.lookahead_index + 1) % len(self.traj_coords)
+
+        self.draw_lookahead_point(marker)
 
     def callback_trajectory(self, data):
         x = [point.x for point in data.polygon.points]
@@ -125,12 +184,7 @@ class CarController:
         self.draw_car_heading(marker)
 
         if self.tree is not None:
-            _, indexes = self.tree.query(
-                [(self.car_position.x, self.car_position.y)])
-            index_closest = indexes[0]
-            self.traj_closest_x = self.traj_coords[index_closest][0]
-            self.traj_closest_y = self.traj_coords[index_closest][1]
-            self.draw_closest_traj_point(marker)
+            self.pure_pursuit(marker)
 
 
 if __name__ == '__main__':
